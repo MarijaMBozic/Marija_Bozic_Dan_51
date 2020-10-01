38247af30185757d30745850015abce9ab3f1a48
MarijaMBozic <60043521+MarijaMBozic@users.noreply.github.com>
2020-10-01 23:08:20 +0200

    code improvment

--- HospitalApp/HospitalApp/SQl_Design/Hospital_Sql.sql
+++ HospitalApp/HospitalApp/SQl_Design/Hospital_Sql.sql
@@ -1,3 +1,5 @@
+
+
 Use Hospital
 
 drop table if exists Request
@@ -52,3 +54,25 @@ ALTER TABLE Patient
 ALTER TABLE Doctor
 	ADD CONSTRAINT DoctorJMBG 
 	CHECK(LEN([DoctorJMBG])=13 AND ([DoctorJMBG] not like '%[^0-9]%'))
+
+go
+
+CREATE VIEW vwDoctor AS
+	SELECT        DoctorId, FullName, DoctorJMBG, BankAccount, Username, DoctorPassword
+    FROM            dbo.Doctor
+
+go
+
+CREATE VIEW vwPatient AS
+	SELECT        PatientId, Fullname, PatientJMBG, NumInsurce, Username, PatientPassword, DoctorId
+	FROM            dbo.Patient
+	
+go
+
+CREATE VIEW vwRequest AS
+	SELECT        dbo.Request.RequestId, dbo.Request.Date, dbo.Request.Reason, dbo.Request.Company, dbo.Request.IsUrgent, dbo.Request.IsApproved, dbo.Request.PatientId, dbo.Request.DoctorId, dbo.Patient.Fullname AS Expr1, 
+                         dbo.Patient.PatientId AS Expr2, dbo.Doctor.DoctorId AS Expr3, dbo.Doctor.FullName AS DoctorFullName
+	FROM            dbo.Request INNER JOIN
+                         dbo.Doctor ON dbo.Request.DoctorId = dbo.Doctor.DoctorId INNER JOIN
+                         dbo.Patient ON dbo.Request.PatientId = dbo.Patient.PatientId AND dbo.Doctor.DoctorId = dbo.Patient.DoctorId
+
